dc75da738ac42fe97e82e5951933b40f530cbbee
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-10-14 12:23:46 +0900

    ?

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
@@ -13,7 +14,8 @@ class TodoController extends Controller
      */
     public function index()
     {
-        //
+        $todo_list = Todo::all();
+        return view('todo/index', compact('todo_list'));
     }
 
     /**
