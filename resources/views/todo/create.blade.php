41bb620e899a8987dec3f4d7df028ca14ea501b7
2019 DSP4101 <2019DSP4101@ntsv6101.com>
2020-11-11 11:31:26 +0900

    z

--- resources/views/todo/create.blade.php
+++ resources/views/todo/create.blade.php
@@ -1,45 +1,15 @@
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
-          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
-    <link rel="stylesheet" href="https://npmcdn.com/flatpickr/dist/themes/material_blue.css">
-    <title>TODO App</title>
-</head>
-<body>
+@extends('layouts.app')
 
-<div class="container">
-    <div class="col-md-12">
-        <h1 class="text-center text-primary py-3">TODO App</h1>
-
-        <h2 class="text-muted py-3">TODO作成</h2>
-        <form method="POST" enctype="multipart/form-data"  action="<?php print($_SERVER['PHP_SELF']) ?>">
-            <div class="form-group">
-                <label for="title">タスク名</label>
-                <input type="text" class="form-control" name="title" id="title" placeholder="タスク名" required>
-            </div>
-            <div class="form-group">
-                <label for="due_date">期限</label>
-                <input type="text" class="form-control" name="due_date" id="due_date" required>
-            </div>
-            <input class="btn btn-primary"  type="submit" name="btn" value="TODOを作成する">
-        </form>
-        <hr>
-        <a href="/todo" class="btn btn-primary">戻る</a>
-
-</div>
-
-<!-- JS, Popper.js, and jQuery -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-</script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
-</script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
-        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
-  </script>
-</body>
-</html>
+@section('content')
+    <div class="container">
+        <div class="col-md-10">
+            <h2 class="text-muted py-3">Todo 作成</h2>
+            <form action="/todo" method="POST">
+                @include('parts.todo.input')
+                <button class="btn btn-primary" type="submit">作成</button>
+            </form>
+            <hr>
+            @include('parts.button.back')
+        </div>
+    </div>
+@endsection 
